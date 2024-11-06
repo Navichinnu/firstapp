476ed29d43a1c671799be587ad6a004dac676e1a
sdorepalli <46470719+sdorepalli@users.noreply.github.com>
2024-11-06 19:26:31 +0530

    bala

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function Footer(){
+    return <div> 
+        <nav class="navbar px-5 bg-secondary fixed-bottom">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      
+      Bootstrap
+    </a>
+  </div>
+</nav>
+         </div>
+}
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Counter from "../fetures/counter/counter";
+import Todolist from "../fetures/todolist/todolist";
+import Todo from "../fetures/todolist/todo";
+function Main(){
+    return (
+        <div>
+     <Todolist></Todolist>
+     <Counter></Counter>
+    
+     </div>
+    )
+}
+export default Main;
